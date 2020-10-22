776d8e32cb912f4ca8b0e7d2a476373ef9ad45f3
Surya909 <73313331+Surya909@users.noreply.github.com>
2020-10-22 23:57:48 +0100

    Update ArrayDemo1.java

--- JavaObject-master/src/javaconcepts/ArrayDemo1.java
+++ JavaObject-master/src/javaconcepts/ArrayDemo1.java
@@ -1,8 +1,6 @@
 package javaconcepts;
 
-/**
- * Created by User on 20/08/2017.
- */
+
 public class ArrayDemo1 {
     public static void main(String args[]) {
         int marks[] = {90,80,91,89,78,45};
