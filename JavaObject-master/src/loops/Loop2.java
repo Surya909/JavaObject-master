1b56053f275d05407659ca69be65dba3de14427b
Surya909 <73313331+Surya909@users.noreply.github.com>
2020-10-23 00:01:35 +0100

    Update Loop2.java

--- JavaObject-master/src/loops/Loop2.java
+++ JavaObject-master/src/loops/Loop2.java
@@ -1,8 +1,6 @@
 package loops;
 
-/**
- * Created by User on 20/08/2017.
- */
+
 public class Loop2 {
 public static void main(String[] args) {
     int marks[] = {90,80,91,89,78,45,45,45,45,234,54,6,345,345,3657,5,65,75,6756,7,464,6,45,656,57,56,765,75,765,7,657};
