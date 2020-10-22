6c02308f5fb8dd481f4a6626c3c48b51b68f9c65
Surya909 <73313331+Surya909@users.noreply.github.com>
2020-10-22 23:55:11 +0100

    Update LogicalOperatorsWithIfElse.java

--- JavaObject-master/src/conditionalStatments/LogicalOperatorsWithIfElse.java
+++ JavaObject-master/src/conditionalStatments/LogicalOperatorsWithIfElse.java
@@ -1,8 +1,6 @@
 package conditionalStatments;
 
-/**
- * Created by User on 13/09/2017.
- */
+
 public class LogicalOperatorsWithIfElse {
     public static void main(String[] args) {
         LogicalOperatorsWithIfElse l1 = new LogicalOperatorsWithIfElse();
