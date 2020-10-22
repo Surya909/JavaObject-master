b0409ede49ca98d52f6492ca85c8cd4f0bf40aa3
Surya909 <73313331+Surya909@users.noreply.github.com>
2020-10-23 00:07:43 +0100

    Update RandomDemo1.java

--- JavaObject-master/src/RandomDemo1.java
+++ JavaObject-master/src/RandomDemo1.java
@@ -1,8 +1,6 @@
 import java.util.Random;
 
-/**
- * Created by User on 20/08/2017.
- */
+
 public class RandomDemo1 {
     public static void main(String[] args) {
         Random random = new Random();
