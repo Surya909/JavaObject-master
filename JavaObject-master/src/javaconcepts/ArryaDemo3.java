7ae441792ef00578c703e31c18b1c9a7525d94fc
Surya909 <73313331+Surya909@users.noreply.github.com>
2020-10-22 23:58:32 +0100

    Update ArryaDemo3.java

--- JavaObject-master/src/javaconcepts/ArryaDemo3.java
+++ JavaObject-master/src/javaconcepts/ArryaDemo3.java
@@ -1,8 +1,6 @@
 package javaconcepts;
 
-/**
- * Created by User on 20/08/2017.
- */
+
 public class ArryaDemo3 {
     public static void main(String[] args) {
         int[][] tableOne = {{23,34,45},{45,67,78,23},{87,45,75}};
