311208d704a8aa389d176fb4a1b0ed5c5513d567
Surya909 <73313331+Surya909@users.noreply.github.com>
2020-10-22 23:45:29 +0100

    Update Calc1.java

--- JavaObject-master/src/Abstraction/Calc1.java
+++ JavaObject-master/src/Abstraction/Calc1.java
@@ -1,8 +1,5 @@
 package Abstraction;
 
-/**
- * Created by User on 04/12/2016.
- */
 public abstract class Calc1 implements MyInterface {
 //    public abstract void method2();
     public void add(int a, int b) {
